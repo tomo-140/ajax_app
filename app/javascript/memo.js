71aeb4b3a896e00abb0f35b645e5265af45ed02b
tomo-140 <75531846+tomo-140@users.noreply.github.com>
2021-06-10 13:42:28 +0900

    メモ投稿機能のフロントエンド実装

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,4 @@
+function memo() {
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
     const formData = new FormData(document.getElementById("form"));
